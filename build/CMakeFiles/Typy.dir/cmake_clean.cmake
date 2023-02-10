2aac8fca035f2862badba427b355561152cb9768
islam <islamee@aucegypt.edu>
2023-02-10 23:36:03 +0200

    added Cmake files and restructured the folder

---
+++ build/CMakeFiles/Typy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Typy.dir/src/Game.cpp.o"
+  "CMakeFiles/Typy.dir/src/Game.cpp.o.d"
+  "CMakeFiles/Typy.dir/src/main.cpp.o"
+  "CMakeFiles/Typy.dir/src/main.cpp.o.d"
+  "Typy"
+  "Typy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Typy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/cmake-build-debug/config.h
@@ -0,0 +1,4 @@
+#pragma once
+#define PROJECT_VERSION_MAJOR 0 
+#define PROJECT_VERSION_MINOR 1 
+#define RESOURCE_PATH "/home/islam/sfml/Typy/content/"
